8b8b78daeda8ca6330a92631196c83748ec744ef
Divyanshi <guptadivyanshi15@gmail.com>
2026-04-03 00:29:42 +0530

    mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import CreatePost from './pages/CreatePost'
 import Feed from './pages/feed.jsx'
 
